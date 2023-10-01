0a44554314beafe421a334fe64a3c18e19905b84
ZzW163010 <146351364+ZzW163010@users.noreply.github.com>
2023-10-01 13:09:18 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<string>
+int main()
+{
+
+	int a = 1;
+	printf("%d", a);
+	/*char arr1[] = "abc";
+	char arr2[] = { 'a','b','c'};
+
+	printf("%d\n", strlen(arr1));
+	printf("%s\n", arr2);*/
+	/*printf("asf");
+
+
+	return 0;*/
+}
+
+
+
+//enum Sex //Ã¶¾Ù³£Á¿
+//{
+//	MALE,
+//	FMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//
+//	
+//
+//	return 0;
+//}
+//
+//
+////int main()
+//{
+//
+//	int a = 0;
+//
+//	scanf("%d", &a);
+//	printf("%d", a);
+//
+//	return 0;
+//}

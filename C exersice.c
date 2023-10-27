160ee45c9c6ef9c77ac5ec87f07b61d539958e0e
ZzW163010 <146351364+ZzW163010@users.noreply.github.com>
2023-10-27 10:45:58 +0800

    Update C exersice.c

--- C exersice.c
+++ C exersice.c
@@ -5,3 +5,17 @@ int main() {
     printf("%#o %#X",1234,1234);
     return 0;
 }
+
+//将一个四位数反向输出
+
+int main() {
+    int nums = 0;
+    scanf("%d",&nums);
+    int num = 0;
+    while(nums){
+        num = nums % 10;
+        nums = nums / 10;
+        printf("%d",num);
+    }
+    return 0;
+}
